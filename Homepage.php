6d2c7135e8a9be7ee194ef48de7213a8fa776654
JHAN_VINCENT_VILLAFRANCA <vjhanvincent26@gmail.com>
2021-02-19 15:47:00 +0800

    2nd commit

--- Homepage.php
+++ Homepage.php
@@ -8,7 +8,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
 <style> 
 html,body {
   height:100%;
@@ -20,12 +19,10 @@ html,body {
   bottom:50%;
   left:50%;
   transform: translate(-50%, -50%);
-
 }  
 #fsize{ 
   font-size:50px;
 }
-
 .a{
   display: block;
   width: 200px;
@@ -41,12 +38,10 @@ html,body {
   position: absolute;
   transition: all .35s;
 }
-
 .a span{
   position: relative;
   z-index: 2;
 }
-
 .a:after{
   position: absolute;
   content: "";
@@ -57,23 +52,18 @@ html,body {
   background: red;
   transition: all .35s;
 }
-
 .a:hover{
   color:white;
   border:5px double red;
 }
-
 .a:hover:after{
   width: 100%;
 }
-
 </style>
-</head>
-    <title>Anime</title>
+
+<title>Anime</title>
 </head>
 <body>
-
-
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
   <a class="navbar-brand" href="#">Anime</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
@@ -94,7 +84,7 @@ html,body {
   </div>  
 </nav>
     <div id = "container">
-    <div><img src="video.gif" alt="logo" style="height:720"></div>
+    <div><img src="video.gif" alt="logo" style ="height:720px;width:100%"></div>
     </div>
     <div id = "center">
         <h1 id = "center" style = "color:red;font-size:70px">A N I M E</h1>
@@ -102,23 +92,14 @@ html,body {
     </div>
 <div>
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
 Why do we use it?
 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-
 Where does it come from?
 Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
 The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
 </div>
-
 <div class="jumbotron jumbotron-fluid bg-dark text-center" >
   <p>Footer</p>
 </div>
-
-
-
-
 </body>
 </html>
