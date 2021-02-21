1858f5b46cd3a707d32180de40fd3686524f5216
JHAN_VINCENT_VILLAFRANCA <vjhanvincent26@gmail.com>
2021-02-21 21:22:12 +0800

    with logo

--- Homepage.php
+++ Homepage.php
@@ -10,18 +10,18 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     
-<title>Anime</title>
-
+<title>Anime Stop</title>
 </head>
 <body>
-
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-<div class = "container"><a class="navbar-brand" href="#">Anime</a>
+<nav class="navbar navbar-expand-sm bg-dark navbar-custom fixed-top">
+<a href="#">
+<img src="logo.png" alt="Logo" style="width:70px;">
+</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
+    <ul class="navbar-nav navbar-custom" id = "navfont">
       <li class="nav-item">
         <a class="nav-link" href="#">Clothes</a>
       </li>
@@ -35,13 +35,13 @@
   </div>
 </nav>
 </div>
-    <div>
-    <div><img src="video.gif" alt="logo" style ="height:935px;width:100%" class = "img-fluid"></div>
-    </div>
-    <div id = "center">
-        <h1 id = "center" style = "color:red;font-size:70px">A N I M E</h1>
-        <a class = "a" href="#" id = "center"><span>Shop Now</span></a>
-    </div>
+<div class="box">
+  <img src="video.gif" class = "img-responsive" alt="header image" style = "width:100%"/>
+  <div class="img-title">
+        <div><h1 id = "center" style = "color:red" class = "example">ANIME STOP</h1></div>
+        <div><a class = "a" href="#" id = "center"><span>Shop Now</span></a></div>
+  </div>
+</div>
 <div class = "container">
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 Why do we use it?
@@ -50,14 +50,11 @@ Where does it come from?
 Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
 The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
 </div>
-<<<<<<< HEAD
+
 <div id = "page-container">
 <div id = "content-wrap">
 <div class="jumbotron jumbotron-fluid bg-dark text-center" style="margin-bottom:0" id = "footer" >
   <p>Footer</p>
-<div class="jumbotron jumbotron-fluid bg-dark text-center" >
-  <footer> footer</footer>
-
 </div>
 </div>
 </div>
