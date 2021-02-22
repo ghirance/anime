69713a59c894970059fd4af3983af01de0a69509
JHAN_VINCENT_VILLAFRANCA <vjhanvincent26@gmail.com>
2021-02-23 03:19:03 +0800

    with footer

--- Homepage.php
+++ Homepage.php
@@ -55,7 +55,6 @@
     <img src="twt.png" alt="">
   </div>
   <!-- Links -->
-
 </nav>
 <!-- Navbar -->
 </div>
@@ -92,12 +91,8 @@
         </a>
         <h5 style = "text-align:center">P300</h5>
       </div>
-
-
-
     </div>
 </div>
-
 <div class = "container">
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 Why do we use it?
