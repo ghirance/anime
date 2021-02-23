b4d01715da83631b8df5bc853b339f7091a31416
Carrysuu <rolandcarreon07@gmail.com>
2021-02-23 17:57:56 +0800

    shop now page

---
+++ shopNowPage.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src = "transparent.js"></script>
+<title>Anime Stop</title>
+</head>
+<body>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-md navbar-light bg-dark fixed-top bg-company-red">
+
+<!-- Collapse button -->
+ <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav11"
+    aria-controls="basicExampleNav11" aria-expanded="false" aria-label="Toggle navigation" style = "padding:0%;border:0px" >
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Logo -->
+  <a class="navbar-brand mx-auto d-block" href="#!">
+    <img src="logos.png" height = "60" style= "padding-right:30px">
+  </a>
+
+  <!-- Links -->
+  <div class="collapse navbar-collapse" id="basicExampleNav11">
+    <!-- Right -->
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a href="#!" class="nav-link waves-effect">
+         Home
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#!" class="nav-link waves-effect">
+         Shop
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#!" class="nav-link waves-effect">
+          Contact
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#!">
+        <img src = "person_outline-24px.svg" style = "padding-top:5px">
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#!" >
+          <img src = "local_mall-24px.svg" style = "padding-top:5px">
+      </li>
+      <li class="nav-item">
+        <a href="#!" >
+          <img src = "search-24px.svg" style = "padding-top:5px">
+        </a>
+      </li>
+    </ul>
+  </div>
+  <!-- Links -->
+</nav>
+<!-- Navbar -->
+</div>
+<div class="box">
+  <img src="video.gif" class = "img-responsive" alt="header image" style = "width:100%">
+  <div class="img-title">
+        <div><h1 id = "center" style = "color:red" class = "example">ANIME STOP</h1></div>
+        <div><a class = "a" href="#" id = "center"><span>Shop Now</span></a></div>
+  </div>
+</div>
+
+</footer>
+<footer class="bg-light text-white ">
+    <div class="container-fluid py-3 ">
+        <div class="row">
+          <div class = "col-md-12" id = "fontF">©Villafranca and Carreon 2020</div>
+        </div>
+</footer>
+</body>
+</html>
