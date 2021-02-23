185893790c9c5f6b82f67270ffd1de2c8ad4d1e5
JHAN_VINCENT_VILLAFRANCA <vjhanvincent26@gmail.com>
2021-02-23 15:54:09 +0800

    more products

--- Homepage.php
+++ Homepage.php
@@ -5,32 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="" href="style.css">
+    <link rel="stylesheet" href="style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src = "transparent.js"></script>
-
 <title>Anime Stop</title>
 </head>
 <body>
 <!-- Navbar -->
-<nav class="navbar navbar-expand-md navbar-light bg-dark fixed-top">
+<nav class="navbar navbar-expand-md navbar-light bg-dark fixed-top bg-company-red">
 
 <!-- Collapse button -->
- <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#basicExampleNav11"
+ <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav11"
     aria-controls="basicExampleNav11" aria-expanded="false" aria-label="Toggle navigation" style = "padding:0%;border:0px" >
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <!-- Logo -->
   <a class="navbar-brand mx-auto d-block" href="#!">
-    <img src="logo.png" height = "60" style= "padding-right:30px">
+    <img src="logos.png" height = "60" style= "padding-right:30px">
   </a>
 
   <!-- Links -->
   <div class="collapse navbar-collapse" id="basicExampleNav11">
-
     <!-- Right -->
     <ul class="navbar-nav ml-auto">
       <li class="nav-item">
@@ -40,73 +38,194 @@
       </li>
       <li class="nav-item">
         <a href="#!" class="nav-link waves-effect">
-          Contact
+         Shop
         </a>
       </li>
       <li class="nav-item">
         <a href="#!" class="nav-link waves-effect">
-          Sign in
+          Contact
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#!">
+        <img src = "person_outline-24px.svg" style = "padding-top:5px">
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#!" >
+          <img src = "local_mall-24px.svg" style = "padding-top:5px">
+      </li>
+      <li class="nav-item">
+        <a href="#!" >
+          <img src = "search-24px.svg" style = "padding-top:5px">
         </a>
       </li>
     </ul>
-  <!-- di ko pa sure kung ano lalagay natin eh dinagdagan ko lang  -->
-    <img src="cart.png" alt="">
-    <img src="fb.png" alt="">
-    <img src="twt.png" alt="">
   </div>
   <!-- Links -->
 </nav>
 <!-- Navbar -->
 </div>
 <div class="box">
-  <img src="video.gif" class = "img-responsive" alt="header image" style = "width:100%"/>
+  <img src="video.gif" class = "img-responsive" alt="header image" style = "width:100%">
   <div class="img-title">
         <div><h1 id = "center" style = "color:red" class = "example">ANIME STOP</h1></div>
         <div><a class = "a" href="#" id = "center"><span>Shop Now</span></a></div>
-  </div>
+  </div>     
 </div>
   <div class = "container">
+  <div class = "d-flex justify-content-center" style = "padding-top:30px">
+  <span id = "fontH">ALL PRODUCTS</span>
+  </div>
     <div class="row" id = "row1">
       <div class="col-sm"><a href = "#!">
-            <img src = "Figures/blue.jpg" class = "mx-auto d-block " >
+            <img src = "Figures/kimono-1.jpg" class = "mx-auto d-block bg-dark" >
             <h3 style = "text-align:center">Tanjiro</h3>
         </a>
         <h5 style = "text-align:center">P300</h5></div>
       <div class="col-sm"><a href = "#!">
-            <img src = "Figures/pink.jpg" class = "mx-auto d-block " >
+            <img src = "Figures/kimono-2.jpg" class = "mx-auto d-block " >
             <h3 style = "text-align:center">Tanjiro</h3>
         </a>
         <h5 style = "text-align:center">P300</h5></div>
       <div class="col-sm">
       <a href = "#!">
-            <img src = "Figures/red.jpg" class = "mx-auto d-block " >
+            <img src = "Figures/kimono-3.jpg" class = "mx-auto d-block " >
             <h3 style = "text-align:center">Tanjiro</h3>
         </a>
         <h5 style = "text-align:center">P300</h5>
       </div>
       <div class="col-sm">
         <a href = "#!">
-            <img src = "Figures/yellow.jpg" class = "mx-auto d-block " >
+            <img src = "Figures/kimono-4.jpg" class = "mx-auto d-block " >
+            <h3 style = "text-align:center">Tanjiro</h3>
+        </a>
+        <h5 style = "text-align:center">P300</h5>
+      </div>
+    </div>
+    <div class="row" id = "row1">
+      <div class="col-sm"><a href = "#!">
+            <img src = "Figures/giyu.jpg" class = "mx-auto d-block " >
+            <h3 style = "text-align:center">Tanjiro</h3>
+        </a>
+        <h5 style = "text-align:center">P300</h5></div>
+      <div class="col-sm"><a href = "#!">
+            <img src = "Figures/nezuko.jpg" class = "mx-auto d-block " >
+            <h3 style = "text-align:center">Tanjiro</h3>
+        </a>
+        <h5 style = "text-align:center">P300</h5></div>
+      <div class="col-sm">
+      <a href = "#!">
+            <img src = "Figures/zenitsu.jpg" class = "mx-auto d-block " >
+            <h3 style = "text-align:center">Tanjiro</h3>
+        </a>
+        <h5 style = "text-align:center">P300</h5>
+      </div>
+      <div class="col-sm">
+        <a href = "#!">
+            <img src = "Figures/tanjiro.jpg" class = "mx-auto d-block " >
+            <h3 style = "text-align:center">Tanjiro</h3>
+        </a>
+        <h5 style = "text-align:center">P300</h5>
+      </div>
+    </div>
+    <div class="row" id = "row1">
+      <div class="col-sm"><a href = "#!">
+            <img src = "Figures/hoodie-1.jpg" class = "mx-auto d-block " >
+            <h3 style = "text-align:center">Tanjiro</h3>
+        </a>
+        <h5 style = "text-align:center">P300</h5></div>
+      <div class="col-sm"><a href = "#!">
+            <img src = "Figures/hoodie-2.jpg" class = "mx-auto d-block " >
+            <h3 style = "text-align:center">Tanjiro</h3>
+        </a>
+        <h5 style = "text-align:center">P300</h5></div>
+      <div class="col-sm">
+      <a href = "#!">
+            <img src = "Figures/hoodie-3.jpg" class = "mx-auto d-block " >
+            <h3 style = "text-align:center">Tanjiro</h3>
+        </a>
+        <h5 style = "text-align:center">P300</h5>
+      </div>
+      <div class="col-sm">
+        <a href = "#!">
+            <img src = "Figures/hoodie-4.jpg" class = "mx-auto d-block " >
+            <h3 style = "text-align:center">Tanjiro</h3>
+        </a>
+        <h5 style = "text-align:center">P300</h5>
+      </div>
+    </div>
+    <div class="row" id = "row1">
+      <div class="col-sm"><a href = "#!">
+            <img src = "Figures/naruto.jpg" class = "mx-auto d-block " >
+            <h3 style = "text-align:center">Tanjiro</h3>
+        </a>
+        <h5 style = "text-align:center">P300</h5></div>
+      <div class="col-sm"><a href = "#!">
+            <img src = "Figures/sasuke.jpg" class = "mx-auto d-block " >
+            <h3 style = "text-align:center">Tanjiro</h3>
+        </a>
+        <h5 style = "text-align:center">P300</h5></div>
+      <div class="col-sm">
+      <a href = "#!">
+            <img src = "Figures/sakura.jpg" class = "mx-auto d-block " >
+            <h3 style = "text-align:center">Tanjiro</h3>
+        </a>
+        <h5 style = "text-align:center">P300</h5>
+      </div>
+      <div class="col-sm">
+        <a href = "#!">
+            <img src = "Figures/gaara.jpg" class = "mx-auto d-block " >
             <h3 style = "text-align:center">Tanjiro</h3>
         </a>
         <h5 style = "text-align:center">P300</h5>
       </div>
     </div>
 </div>
+<div class = "d-flex justify-content-center" style = "padding-top:30px">
+<a href="#" id = "bd">Shop All Products</a>
+</div>
 <div class = "container">
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-Why do we use it?
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-Where does it come from?
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+  <h2></h2>
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
+a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining
+essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
+a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining
+essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 </div>
-<div id = "page-container">
-<div id = "content-wrap">
-<div class="jumbotron jumbotron-fluid bg-dark text-center" style="margin-bottom:0" id = "footer" >
-  <p>Footer</p>
-</div>
-</div>
-</div>
+
+
+
+<!-- Footer -->
+<footer class="bg-light text-white bg-company-red ">
+    <div class="container py-5">
+        <div class="row " >
+            <div class="col-xl-3"><img src="logos.png" height = "60" style= "padding-right:30px"></div>
+            <div class="col-xl-3"></div>
+            <div class="col-xl-3"></div>
+            <div class="col-xl-3" id = "fontF">Let's Keep In Touch
+                <div><a href = "#!"> <img src="fb.png" style = "height:24px"></a>
+                <a href = "#!"> <img src="twt.png" style = "height:24px"></a>
+                </div>  
+            </div>
+
+            
+          </div>
+          
+        </div>
+        
+       
+</footer>
+<footer class="bg-light text-white ">
+    <div class="container-fluid py-3 ">
+        <div class="row">
+          <div class = "col-md-12" id = "fontF">©Villafranca and Carreon 2020</div>
+        </div>
+</footer>
 </body>
 </html>
